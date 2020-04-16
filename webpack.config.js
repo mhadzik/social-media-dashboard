2264327f9b886b6d3c045331f819e370057c1399
Mikolaj Hadzik <mikolaj.hadzik@gmail.com>
2020-04-16 23:12:30 +0200

    Config Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,29 @@
 const {
     resolve
 } = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/scripts/index.js',
     output: {
         path: resolve(__dirname, "dist/"),
         filename: 'bundle.js'
     },
-
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: resolve(__dirname, "dist/"),
+    },
+    plugins: [
+        // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+    ],
     module: {
         rules: [{
             test: /.(scss)$/i,
